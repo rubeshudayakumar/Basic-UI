a084378f094c76ef277caa05ccbae393899af16a
rubeshudayakumar <rubesh.udayakumar@cdw.com>
2023-02-24 12:03:25 +0530

    COMMIT

--- Math Kids/assets/js/script.js
+++ Math Kids/assets/js/script.js
@@ -1,8 +1,32 @@
+// code for putting tick mark
+var shapesList = document.querySelectorAll(".shapeContainer .shape");
 
+var shapeName = "triangle";
 
-var shapesList = document.querySelectorAll(".shapeContainer shape");
-
-
+// code for selecting the shapes
 shapesList.forEach((val) => {
-    console.log(val);
+    let selectedShape = "triangle";
+    val.addEventListener("click",(event)=>{
+        event.target.innerHTML = "<div class='tickMark'></div>";
+        selectedShape = event.target;
+        shapesList.forEach((val)=>{
+            if(val!=selectedShape){
+                val.innerHTML = "";
+            }
+        });
+        shapeName = selectedShape.outerHTML;
+        shapeName = shapeName.split("shape")[0].split("=")[1].slice(1).trim();
+        if(shapeName=="triangle"){
+            shapesList[1].children[0].classList.add("alignTick");
+        }
+        document.querySelector(".chooseShapeSection button").style.visibility = "visible";
+    });
 });
+
+// code for triggering next button click event
+document.querySelector(".nextButton").addEventListener("click",(event)=>{
+    let currentSection =  document.querySelector(".calculationArea");
+    currentSection.removeChild(currentSection.childNodes[3]);
+    
+});
+
