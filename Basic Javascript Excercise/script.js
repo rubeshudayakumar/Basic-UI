68aa7aa6a8ae442f943e71467993a7460d1170b2
rubeshudayakumar <rubesh.udayakumar@cdw.com>
2023-02-27 10:41:11 +0530

    COMMIT

--- Basic Javascript Excercise/script.js
+++ Basic Javascript Excercise/script.js
@@ -1,6 +1,6 @@
 // Section 1 : Play with variables
 
-console.log("<----- Section 1 : Play with variables ----->")
+console.log("<----- Section 1 : Play with variables ----->");
 console.log(1+2); // 3
 console.log("apple"+"orange"); // appleorange
 console.log(1+2+"apple"); //3apple
@@ -12,15 +12,15 @@ console.log(2=="2"); //true
 
 //Section 2 : Play with arrays 
 
-console.log("<----- Section 2 : Play with arrays ----->")
+console.log("<----- Section 2 : Play with arrays ----->");
 // creating an array with a name of 10 cricket players
-cricketPlayers = ["Watson","Faf","Rayudu","Raina","Dhoni","Jadeja","Bravo","Morkel","Tahir","Ashwin","Chahar"]
+cricketPlayers = ["Watson","Faf","Rayudu","Raina","Dhoni","Jadeja","Bravo","Morkel","Tahir","Ashwin","Chahar"];
 
 // because of the injury the first player is being removed
-cricketPlayers.shift()
+cricketPlayers.shift();
 
 // finding the number of players remaining
-console.log(cricketPlayers.length)
+console.log(cricketPlayers.length);
 
 // adding another player to the top of the list 
 cricketPlayers.unshift("Hussey");
@@ -30,13 +30,13 @@ cricketPlayers.sort();
 console.log(cricketPlayers);
 
 // assigning a randomjersy number to the individual players
-cricketPlayers.forEach((val,i,a)=> {
-   console.log(val+"-"+Math.round(Math.random()*1000));
-    a[i]=val+"-"+Math.round(Math.random()*1000);
+cricketPlayers.forEach((value,index,array)=> {
+   console.log(value+"-"+Math.round(Math.random()*1000));
+    array[index]=value+"-"+Math.round(Math.random()*1000);
 });
 
 // printing in uppercase and storing it in different place
-printingJersy = []
+printingJersy = [];
 cricketPlayers.forEach((val)=> {
     printingJersy.push(val.toUpperCase());
 });
@@ -46,7 +46,7 @@ console.log("<----- Section 3 : Play with functions ----->")
 
 // display 1 to 100
 function displayNumbers() {
-    numbers = "";
+    let numbers = "";
     for(let i=1;i<=100;i++){
         numbers+=(i+" ");
     }
@@ -72,26 +72,26 @@ function celciusToFaranheit(celcius) {
 console.log(celciusToFaranheit(45));
 
 // finding average of numbers 
-function average(arr){
-    total = 0;
-    len = arr.length;
+function computeAverage(numbers){
+    let total = 0;
+    let len = arr.length;
     for(i=0;i<len;i++){
-        total+=arr[i];
+        total+=numbers[i];
     }
     return total/len;
 }
-arr = [1,2,3,4,5,6,7,8,9,10]
-console.log(average(arr));
+var numbers = [1,2,3,4,5,6,7,8,9,10]
+console.log(computeAverage(numbers));
 
 // function to reverse a string
-function reverse(str){
-    reversedStr = "";
+function reverseTheString(str){
+    let reversedString = "";
     for(i=str.length-1;i>=0;i--){
-        reversedStr+=str[i];
+        reversedString+=str[i];
     }
-    return reversedStr;
+    return reversedString;
 }
-console.log(reverse("Hello World!"));
+console.log(reverseTheString("Hello World!"));
 
 
 
