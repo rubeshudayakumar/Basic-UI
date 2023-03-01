2a3e6fda4d8225b23d7018dd1daef3c36c174885
rubeshudayakumar <rubesh.udayakumar@cdw.com>
2023-03-02 00:44:39 +0530

    COMMIT

---
+++ Sirius Locations/assets/js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+    // initially selecting the about us tab and this about us section will be displayed
+    var prevSelectedTab = $(".active");
+    var prevSelectedSection = $(".about-us-section");
+
+    // adding click event listeners for all the tabs
+    $(".tab").click(function (e) { 
+
+        // toggling the tab color to the clicked tab
+        prevSelectedTab.toggleClass("active");
+        $(this).toggleClass("active");
+        prevSelectedTab = $(this);
+        
+        // toggling the section with the selected tab
+        prevSelectedSection.toggleClass("tab-display-control");
+        var selectedTabName = $(this)[0].outerHTML.toString().split("tab")[1].trim();
+        $("."+selectedTabName+"section").toggleClass("tab-display-control");
+        prevSelectedSection = $("."+selectedTabName+"section");
+    });
+});
