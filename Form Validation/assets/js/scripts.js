e509aacf86c481925c8e4b7c0c2de422dc27108c
rubeshudayakumar <rubesh.udayakumar@cdw.com>
2023-03-03 17:44:06 +0530

    commit

--- Form Validation/assets/js/scripts.js
+++ Form Validation/assets/js/scripts.js
@@ -13,30 +13,36 @@ function validate(){
     let cvv = document.getElementById("cvv-number");
 
 
-
     // function validateInput()
 
     function validateInput(inputObject,regEx,errorMessageEmpty,errorMessageInValid,selectorString){
         var errorMessageSpan = document.querySelector(selectorString);
 
+        
+
         if(inputObject.value.trim()==""){
             errorMessageSpan.innerHTML = errorMessageEmpty;
             inputObject.style.borderColor = "#A80000";
+            errorMessageSpan.style.display = "inline";
+            
             isValid = false;
         }
         else if((regEx).test(inputObject.value)==false){
             errorMessageSpan.innerHTML = errorMessageInValid;
             inputObject.style.borderColor = "#A80000";
+            errorMessageSpan.style.display = "inline";
+            
             isValid = false;
         }else{
             errorMessageSpan.innerHTML = "";
+            errorMessageSpan.style.display = "none";
             inputObject.style.borderColor = "#CCCCCC";
         }
     }
 
-    validateInput(firstName,/^[a-zA-Z]{1,30}$/g,"First Name is required","First Name is not valid",".first-section > .error-message");
-    validateInput(lastName,/^[a-zA-Z]{1,30}$/g,"Last Name is required","Last Name is not valid",".second-section > .error-message");
-    validateInput(email,/^[A-z]+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,"Email Address is required","Email Address is not valid","#email-id ~ .error-message");
+    validateInput(firstName,/^[a-zA-Z]{1,30}$/g,"First Name is required","First Name is not valid","#first-name ~ .error-message");
+    validateInput(lastName,/^[a-zA-Z]{1,30}$/g,"Last Name is required","Last Name is not valid","#last-name ~ .error-message");
+    validateInput(email,/^[a-z][a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]+$/,"Email Address is required","Email Address is not valid","#email-id ~ .error-message");
     validateInput(contactNumber,/^[0-9]{10}$/,"Contact Number is required","Contact Number is not valid","#contact-number ~ .error-message");
     validateInput(PINCode,/^[0-9]{6}$/,"PIN Code is required","PIN Code is not valid","#pin-code ~ .error-message");
     validateInput(cardNumber,/^[0-9]{16}$/,"Card Number is required","Card Number is not valid","#card-number ~ .error-message");
