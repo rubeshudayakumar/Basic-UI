f8c846610ae9c5a9f9da0d1d473d2d5f55be30a2
rubeshudayakumar <rubesh.udayakumar@cdw.com>
2023-03-10 17:18:07 +0530

    commit

--- Blender Movies Jquery/assets/js/scripts.js
+++ Blender Movies Jquery/assets/js/scripts.js
@@ -1,28 +1,33 @@
-var posters;
-
-var video;
-
 $(document).ready(function () {
+    var posters;
+
+    var video;
+
+    function ajaxCall(url,loadingSelector){
+        let data;
+        $.ajax({
+            async: false,
+            type: "get",
+            url: url,
+            data: "data",
+            dataType: "json", 
+    
+            // showing loading icon
+            beforeSend: function(){
+                $(loadingSelector).show();
+            },
+    
+            // changing the video variable and hiding the loading screen with the success function
+            success: function (response) {
+                data = response;
+                $(loadingSelector).hide();
+            }
+        })
+        return data;
+    }
 
     // loading the video data from the API
-    $.ajax({
-        async: false,
-        type: "get",
-        url: "https://mocki.io/v1/4da47fc5-bbf3-4e41-b35f-c88a584bc4b0",
-        data: "data",
-        dataType: "json", 
-
-        // showing loading icon
-        beforeSend: function(){
-            $("#video-loading").show();
-        },
-
-        // changing the video variable and hiding the loading screen with the success function
-        success: function (response) {
-            video = response;
-            $("#video-loading").hide();
-        }
-    })
+    video = ajaxCall("https://mocki.io/v1/4da47fc5-bbf3-4e41-b35f-c88a584bc4b0","#video-loading");
 
     // setting the profile picture and name 
     $(".profile-section img").attr("src","./assets/"+video.comments[3].image);
@@ -34,24 +39,7 @@ $(document).ready(function () {
     $(".video-description p").text(video.description);
     
     // getting the posters data with the help of the API
-    $.ajax({
-        async: false,
-        type: "get",
-        url: "https://mocki.io/v1/8c9b378b-d248-4203-93b0-b8e7659ac346",
-        data: "data",
-        dataType: "json", 
-
-        // showing loading icon 
-        beforeSend : function(){
-            $("#poster-loading").show();
-        },
-
-        // changing the posters variable and hiding the loading icon
-        success: function (response) {
-            posters = response;
-            $("#poster-loading").hide();
-        }
-    });
+    posters = ajaxCall("https://mocki.io/v1/8c9b378b-d248-4203-93b0-b8e7659ac346","#poster-loading");
 
     // appending the posters to the dom
     posters.forEach((poster) => {
@@ -65,32 +53,25 @@ $(document).ready(function () {
     // including the video comments
     video.comments.forEach((commentItem) => {
         // creating comments container
-        var commentContainer = document.createElement("div");
-        commentContainer.className = "comment-container";
-        $(".comments-section").append(commentContainer);
+        $commentContainer = $("<div>");
+        $commentContainer.attr("class","comment-container");
 
-        // creating commenter profile image
-        var commenterImg = document.createElement("img");
-        commenterImg.src = "./assets/"+commentItem.image;
-        commenterImg.alt = commentItem.name;
-        commentContainer.appendChild(commenterImg);
+         // creating commenter profile image
+        $commentContainer.append($("<img>").attr("src","./assets/"+commentItem.image));
 
         // creating the comment div
-        var comment = document.createElement("div");
-        comment.className = "comment";
+        $comment = $("<div>").attr("class","comment");
 
         // creating the commentors name in h5 tag
-        var commenterName = document.createElement("h5");
-        commenterName.innerText = commentItem.name;
-        comment.appendChild(commenterName);
-        
-        // creating the comment content in the p tag
-        var commentDescription = document.createElement("p");
-        commentDescription.innerHTML = commentItem.comment;
-        comment.appendChild(commentDescription);
+        $comment.append($("<h5>").text(commentItem.name));
 
+        // creating the comment content in the p tag
+        $comment.append($("<p>").text(commentItem.comment));
+        
         // appending it to the comment container
-        commentContainer.appendChild(comment);
+        $commentContainer.append($comment);
+        $(".comments-section").append($commentContainer);
+
     });
 });
 
